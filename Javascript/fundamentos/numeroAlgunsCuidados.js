775d0aedbfcb82c6976bd0443b58e8da44f184a1
Lucaslva580 <lucasaraujo580@gmail.com>
2020-12-01 22:05:09 -0300

    Add strings e mais exempllos de number

--- Javascript/fundamentos/numeroAlgunsCuidados.js
+++ Javascript/fundamentos/numeroAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7/0)//funfa
 console.log("10"/2) //funfa
+console.log("3"+2) //concatena
 console.log("Show"*2) // nao funfa
 console.log(0.1 + 0.7) //impreciso
 //console.log(10.toString())
---
+++ Javascript/fundamentos/strings.js
@@ -0,0 +1,16 @@
+const escola = "cod3r"
+
+console.log(escola.charAt(4)) // quarta letra começando do ídice 0
+console.log(escola.charAt(5)) // vazio
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log('Escola '+escola+"!")
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana, Maria, Pedro'.split(','))
+
