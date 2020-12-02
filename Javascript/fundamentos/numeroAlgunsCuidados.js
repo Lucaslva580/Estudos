ec5153778c17a787076c51d17a936f274d79d091
Lucaslva580 <lucasaraujo580@gmail.com>
2020-12-01 21:41:37 -0300

    Adicionado arquivos de tipagem e Number

---
+++ Javascript/fundamentos/numeroAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0)//funfa
+console.log("10"/2) //funfa
+console.log("Show"*2) // nao funfa
+console.log(0.1 + 0.7) //impreciso
+//console.log(10.toString())
+console.log((10.435).toFixed(2))
