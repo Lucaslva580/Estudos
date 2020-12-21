f81b3e6b25f4925def47c0af281e973ccd07494e
Lucaslva580 <lucasaraujo580@gmail.com>
2020-12-21 20:33:09 -0300

    Um pouco de Var e Let

--- Javascript/fundamentos/funcao02.js
+++ Javascript/fundamentos/funcao02.js
@@ -13,4 +13,7 @@ console.log(soma(2,3))
 
 //retorno implícito
 const subtracao = (a,b) => a - b
-console.log(subtracao(2,3))
+console.log(subtracao(3,2))
+
+const imprimir2 = a => console.log(a)
+imprimir2("Legal!!!")
